fa1e6f57ff7931f6040bdace655aede83db4bc19
Marlon Pacheco <marlonpacheco@MacBook-Pro.local>
2020-10-01 16:12:53 -0500

    troubleshooting button click error. saving progress

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,7 +16,7 @@ router.get("/", function (req, res) {
 })
 
 router.post("/api/burgers", function (req, res) {
-  cat.create([
+  burger.create([
     "name", "munched"
   ], [
     req.body.name, req.body.sleepy
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     munched: req.body.munched
   }, condition, function (result) {
     if (result.changedRows == 0) {
@@ -46,7 +46,7 @@ router.put("/api/burgers/:id", function (req, res) {
 router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function (result) {
+  burger.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,7 +1,20 @@
 //Munch button on-click function
-$(function() {
-$(".munchBtn").on("click", function(){
-    let id = $(this).data("id");
-    let consumed = $(this).data("")
-})
+$(function () {
+    $(".munchBtn").on("click", function () {
+        let id = $(this).data("id");
+        let consumed = $(this).data("data-munched");
+
+        let burgerStatus = {
+            munched: consumed
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: burgerStatus
+        }).then(
+            function () {
+                location.reload()
+            }
+        )
+    })
 })
